197cc3d75fd4536f6d206241a441d7bf85164c64
Rodrigo <rodrigolimapereira@alu.ufc.br>
2024-08-20 18:10:10 -0300

    Definindo Schema do modelo Pokemon

---
+++ src/models/Pokemon.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose"
+
+const pokemonSchema = new mongoose.Schema({
+    
+    id: {type: mongoose.Schema.Types.ObjectId},
+
+    nome: {
+        type: String,
+        required: true
+    },
+
+    numero: {
+        type: Number,
+        required: true
+    },
+
+    tipo: {
+        type: String,
+        enum: [ "Normal",
+                "Fire",
+                "Water",
+                "Grass",
+                "Flying",
+                "Fighting",
+                "Poison",
+                "Electric",
+                "Ground",
+                "Rock",
+                "Psychic",
+                "Ice",
+                "Bug",
+                "Ghost",
+                "Steel",
+                "Dragon",
+                "Dark",
+                "Fairy" ],
+        required: true
+    },
+
+    imagem: {
+        type: String,
+        required: true
+    }
+
+}, {versionKey: false})
