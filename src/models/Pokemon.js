75c886facaa62a067fc4ddb9e7fa37d3779cdbed
Rodrigo <rodrigolimapereira@alu.ufc.br>
2024-08-25 18:35:08 -0300

    Implementando validação de dados nas rotas

--- src/models/Pokemon.js
+++ src/models/Pokemon.js
@@ -1,43 +1,52 @@
 import mongoose from 'mongoose';
+import "./globalValidator.js"
 
 // Define o Schema (estrutura) de um documento no banco de dados
 const pokemonSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
     name: {
         type: String,
-        required: true
+        cast: "O campo {PATH} deve receber uma string válida",
+        min: [0, "O valor {VALUE} é menor que o número permitido para o campo {PATH}"],
+        required: [true, "O campo {PATH} é obrigatório"]
     },
     number: {
         type: Number,
-        required: true
+        cast: "O campo {PATH} deve receber um número válido",
+        required: [true, "O campo {PATH} é obrigatório"]
     },
     type: {
         type: String,
-        enum: [
-            "Normal",
-            "Fire",
-            "Water",
-            "Grass",
-            "Flying",
-            "Fighting",
-            "Poison",
-            "Electric",
-            "Ground",
-            "Rock",
-            "Psychic",
-            "Ice",
-            "Bug",
-            "Ghost",
-            "Steel",
-            "Dragon",
-            "Dark",
-            "Fairy"
-        ],
-        required: true
+        cast: "O campo {PATH} deve receber uma string válida",
+        enum: {
+            values: [
+                "Normal",
+                "Fire",
+                "Water",
+                "Grass",
+                "Flying",
+                "Fighting",
+                "Poison",
+                "Electric",
+                "Ground",
+                "Rock",
+                "Psychic",
+                "Ice",
+                "Bug",
+                "Ghost",
+                "Steel",
+                "Dragon",
+                "Dark",
+                "Fairy"
+            ],
+            message: "O tipo {VALUE} não é válido"
+        },
+        required: [true, "O campo {PATH} é obrigatório"]
     },
     image: {
         type: String,
-        required: true
+        cast: "O campo {PATH} deve receber uma string válida",
+        required: [true, "O campo {PATH} é obrigatório"]
     }
 }, { versionKey: false });
 
